c4c796dd2b6856830d71011bdbb9e9da2154f1aa
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-10-22 19:55:32 +0200

    Declare response var at module start

--- plugins/modules/lbry_version.py
+++ plugins/modules/lbry_version.py
@@ -55,6 +55,8 @@ def main():
     host = module.params['host']
     port = module.params['port']
 
+    response = {}
+
     try:
         url = lbry_build_url(protocol, host, port)
         payload = {
